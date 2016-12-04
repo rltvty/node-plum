e2108341f81431d27a00b8f48be90815c498b121
Eric Pinzur <ericpinzur@otherinbox.com>
2016-12-04 15:38:30 -0600

    added room list endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var plum_config = require('../plum-config');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).json({'index':'nachos'});
+  res.status(200).send(
+      "GET /rooms               List of rooms that can be controlled\n"
+  );
+});
+
+router.get('/rooms', function(req, res, next) {
+    res.status(200).json(plum_config.rooms());
 });
 
 module.exports = router;
