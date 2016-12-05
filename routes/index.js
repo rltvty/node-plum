f96345967bbedcb9db9dedb7210928ac7b0817ab
Eric Pinzur <ericpinzur@otherinbox.com>
2016-12-04 19:22:04 -0600

    get status and set level

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var plum_config = require('../plum-config');
+var plum_request = require('../plum-request');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).send(
-      "GET /rooms               List of rooms that can be controlled\n"
+      "GET /rooms\n" +
+      "GET /room/:room_name/status\n"
   );
 });
 
 router.get('/rooms', function(req, res, next) {
     res.status(200).json(plum_config.rooms());
+});
+
+router.get('/rooms/:room_name/status', function(req, res, next) {
+    var room_props = plum_config.room_props(req.params.room_name);
+    if (room_props == null) {
+        res.status(404).json({'error' : req.params.room_name + ' not found'});
+    } else {
+        var house_token = plum_config.house_token();
+        plum_request.status(house_token, room_props,
+            function callback(error, response, body) {
+                if (!error) {
+                    if (response.statusCode == 200) {
+                        res.status(200).json({level:body.level, power:body.power});
+                    } else {
+                        res.status(response.statusCode).send(body);
+                    }
+                } else {
+                    res.status(500).send(error)
+                }
+            });
+    }
+});
+
+router.post('/rooms/:room_name/level/:level', function(req, res, next) {
+    var room_props = plum_config.room_props(req.params.room_name);
+    var level = parseInt(req.params.level);
+    if (room_props == null) {
+        res.status(404).json({'error' : req.params.room_name + ' not found'});
+    } else {
+        var house_token = plum_config.house_token();
+        plum_request.setLevel(house_token, room_props, level,
+            function callback(error, response, body) {
+                if (!error) {
+                    res.status(response.statusCode).send(body);
+                } else {
+                    res.status(500).send(error)
+                }
+            });
+    }
 });
 
 module.exports = router;
