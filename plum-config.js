e7c4bbde37e5281c250485dd632f7fe6b9e11c70
Eric Pinzur <ericpinzur@otherinbox.com>
2017-02-13 04:19:49 -0600

    added controls endpoints, moved watchers to control_ids

--- plum-config.js
+++ plum-config.js
@@ -5,9 +5,32 @@ module.exports.rooms = function() {
     var rooms = [];
     for(var i in config.rooms) {
         var room = config.rooms[i];
-        rooms.push(room.room_name);
+        var lightpads = [];
+        for (var j in room.lightpads) {
+            var lightpad = room.lightpads[j];
+            lightpads.push(lightpad.lightpad_hostname);
+        }
+        rooms.push({
+            'name' : room.room_name,
+            'controls' : lightpads
+        });
     }
     return rooms;
+};
+
+module.exports.controls = function() {
+    var lightpads = [];
+    for(var i in config.rooms) {
+        var room = config.rooms[i];
+        for (var j in room.lightpads) {
+            var lightpad = room.lightpads[j];
+            lightpads.push({
+                'id' : lightpad.lightpad_hostname,
+                'room_name' : room.room_name
+            });
+        }
+    }
+    return lightpads;
 };
 
 module.exports.room_props = function(room_name) {
@@ -23,6 +46,26 @@ module.exports.room_props = function(room_name) {
                 }
             }
             break;
+        }
+    }
+    return null
+};
+
+module.exports.control_props = function(lightpad_hostname) {
+    for(var i in config.rooms) {
+        var room = config.rooms[i];
+        if ('lightpads' in room && room.lightpads.length > 0) {
+            for (var j in room.lightpads) {
+                var lightpad = room.lightpads[j];
+                if (lightpad.lightpad_hostname === lightpad_hostname) {
+                    return {
+                        load_id: room.load_id,
+                        ip: lightpad.lightpad_ip,
+                        port: lightpad.lightpad_port,
+                        host: lightpad.lightpad_hostname
+                    }
+                }
+            }
         }
     }
     return null
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,12 @@ var plum_request = require('../plum-request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).send(
-      "GET /rooms\n" +
-      "GET /room/:room_name/status\n" +
-      "POST /rooms/:room_name/level/:level\n" +
+      "GET  /rooms\n" +
+      "GET  /rooms/:room_name\n" +
+      "POST /rooms/:room_name?level\n" +
+      "GET  /controls\n" +
+      "GET  /controls/:control_id\n" +
+      "POST /controls/:control_id?level\n" +
       "\n"
   );
 });
@@ -17,43 +20,79 @@ router.get('/rooms', function(req, res, next) {
     res.status(200).json(plum_config.rooms());
 });
 
-router.get('/rooms/:room_name/status', function(req, res, next) {
-    var room_props = plum_config.room_props(req.params.room_name);
-    if (room_props == null) {
-        res.status(404).json({'error' : req.params.room_name + ' not found'});
-    } else {
-        var house_token = plum_config.house_token();
-        plum_request.status(house_token, room_props,
-            function callback(error, response, body) {
-                if (!error) {
-                    if (response.statusCode == 200) {
-                        res.status(200).json({level:body.level, power:body.power});
-                    } else {
-                        res.status(response.statusCode).send(body);
-                    }
-                } else {
-                    res.status(500).send(error)
-                }
-            });
-    }
+router.get('/rooms/:room_name', function(req, res, next) {
+    getStatus(getRoomProps(req), res);
 });
 
-router.post('/rooms/:room_name/level/:level', function(req, res, next) {
+router.post('/rooms/:room_name', function(req, res, next) {
+    setLevel(getRoomProps(req), getLevel(req), res);
+});
+
+router.get('/controls', function(req, res, next) {
+    res.status(200).json(plum_config.controls());
+});
+
+router.get('/controls/:control_id', function(req, res, next) {
+    getStatus(getControlProps(req), res);
+});
+
+router.post('/controls/:control_id', function(req, res, next) {
+    setLevel(getControlProps(req), getLevel(req), res);
+});
+
+function getRoomProps(req) {
     var room_props = plum_config.room_props(req.params.room_name);
-    var level = parseInt(req.params.level);
     if (room_props == null) {
         res.status(404).json({'error' : req.params.room_name + ' not found'});
     } else {
-        var house_token = plum_config.house_token();
-        plum_request.setLevel(house_token, room_props, level,
-            function callback(error, response, body) {
-                if (!error) {
-                    res.status(response.statusCode).send(body);
-                } else {
-                    res.status(500).send(error)
-                }
-            });
+        return room_props;
     }
-});
+}
+
+function getControlProps(req) {
+    var control_props = plum_config.control_props(req.params.control_id);
+    if (control_props == null) {
+        res.status(404).json({'error' : req.params.control_id + ' not found'});
+    } else {
+        return control_props;
+    }
+}
+
+function getLevel(req) {
+    var level = parseInt(req.body.level);
+    if (level < 0 || level > 255) {
+        res.status(400).json({'error' : 'level must be between 0 and 255'});
+    } else {
+        return level;
+    }
+}
+
+function setLevel(props, level, res) {
+    var house_token = plum_config.house_token();
+    plum_request.setLevel(house_token, props, level,
+        function callback(error, response, body) {
+            if (!error) {
+                res.status(response.statusCode).send(body);
+            } else {
+                res.status(500).send(error)
+            }
+        });
+}
+
+function getStatus(props, res) {
+    var house_token = plum_config.house_token();
+    plum_request.status(house_token, props,
+        function callback(error, response, body) {
+            if (!error) {
+                if (response.statusCode == 200) {
+                    res.status(200).json({level:body.level, power:body.power});
+                } else {
+                    res.status(response.statusCode).send(body);
+                }
+            } else {
+                res.status(500).send(error)
+            }
+        });
+}
 
 module.exports = router;
